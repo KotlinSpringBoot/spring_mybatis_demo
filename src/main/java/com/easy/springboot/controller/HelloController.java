e9faf73d131432fe9b7524e45b70a7f2b2729440
jack <guangjian.cgj@alibaba-inc.com>
2018-01-07 03:53:42 +0800

    vm

--- src/main/java/com/easy/springboot/controller/HelloController.java
+++ src/main/java/com/easy/springboot/controller/HelloController.java
@@ -1,24 +1,14 @@
 package com.easy.springboot.controller;
 
-import com.easy.springboot.model.Role;
-import com.easy.springboot.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
-@RequestMapping("/role")
-public class RoleController {
-    @Autowired
-    RoleService roleService;
+public class HelloController {
 
-    @RequestMapping("/list")
-    @ResponseBody
-    List<Role> list() {
-        return roleService.listAll();
+    @RequestMapping("/hello")
+    String hello() {
+        return "hello";
     }
 
 }
